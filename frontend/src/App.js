5190af5269c6fd648160e5bdd6af7b5ba846cf47
moldir <moldirshb@gmail.com>
2022-09-14 09:34:00 -0500

    amazon navbar created

---
+++ frontend/src/App.js
@@ -0,0 +1,17 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <a href="/">amazon</a>
+      </header>
+      <main>
+        list products
+      </main>
+
+    </div>
+  );
+}
+
+export default App;
